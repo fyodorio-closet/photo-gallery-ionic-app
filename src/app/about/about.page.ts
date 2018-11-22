9b72e400a544772f4991c0b686d8a200aec2523f
loenko <loenko.js@gmail.com>
2018-11-22 16:47:38 +0500

    Add camera functionality and corresponding plugins/packages

--- src/app/about/about.page.ts
+++ src/app/about/about.page.ts
@@ -1,8 +1,30 @@
 import { Component } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-about',
   templateUrl: 'about.page.html',
   styleUrls: ['about.page.scss']
 })
-export class AboutPage {}
+export class AboutPage {
+    currentImage: any;
+    constructor(public navCtrl: NavController, private camera: Camera) {
+    }
+
+    takePicture() {
+        const options: CameraOptions = {
+            quality: 100,
+            destinationType: this.camera.DestinationType.DATA_URL,
+            encodingType: this.camera.EncodingType.JPEG,
+            mediaType: this.camera.MediaType.PICTURE
+        };
+
+        this.camera.getPicture(options).then((imageData) => {
+            this.currentImage = 'data:image/jpeg;base64,' + imageData;
+        }, (err) => {
+            // Handle error
+            console.log('Camera issue:' + err);
+        });
+    }
+}
